2977d228fe97c7da6d712230202a17a51d23e5bb
Dawen Liang <dl2771@columbia.edu>
2016-02-14 15:26:38 -0500

    added more documentation

--- src/expomf.py
+++ src/expomf.py
@@ -27,6 +27,35 @@ class ExpoMF(BaseEstimator, TransformerMixin):
     def __init__(self, n_components=100, max_iter=10, batch_size=1000,
                  init_std=0.01, n_jobs=8, random_state=None, save_params=False,
                  save_dir='.', early_stopping=False, verbose=False, **kwargs):
+        '''
+        Exposure matrix factorization
+
+        Parameters
+        ---------
+        n_components : int
+            Number of latent factors
+        max_iter : int
+            Maximal number of iterations to perform
+        batch_size: int
+            Batch size to perform parallel update
+        init_std: float
+            The latent factors will be initialized as Normal(0, init_std**2)
+        n_jobs: int
+            Number of parallel jobs to update latent factors
+        random_state : int or RandomState
+            Pseudo random number generator used for sampling
+        save_params: bool
+            Whether to save parameters after each iteration
+        save_dir: str
+            The directory to save the parameters
+        early_stopping: bool
+            Whether to early stop the training by monitoring performance on
+            validation set
+        verbose : bool
+            Whether to show progress during model fitting
+        **kwargs: dict
+            Model hyperparameters
+        '''
         self.n_components = n_components
         self.max_iter = max_iter
         self.batch_size = batch_size
@@ -46,6 +75,24 @@ class ExpoMF(BaseEstimator, TransformerMixin):
         self._parse_kwargs(**kwargs)
 
     def _parse_kwargs(self, **kwargs):
+        ''' Model hyperparameters
+
+        Parameters
+        ---------
+        lambda_theta, lambda_beta: float
+            Regularization parameter for user and item factors. Default value
+            1e-5. Since in implicit feedback all the n_users-by-n_items data
+            points are used for training, overfitting is almost never an issue
+        lambda_y: float
+            inverse variance on the observational model. Default value 1.0
+        init_mu: float
+            All the \mu_{i} will be initialized as init_mu. Default value is
+            0.01. This number should change according to the sparsity of the
+            data (sparser data with smaller init_mu). In the experiment, we
+            select the value from validation set
+        a, b: float
+            Prior on \mu_{i} ~ Beta(a, b)
+        '''
         self.lam_theta = float(kwargs.get('lambda_theta', 1e-5))
         self.lam_beta = float(kwargs.get('lambda_beta', 1e-5))
         self.lam_y = float(kwargs.get('lam_y', 1.0))
@@ -54,6 +101,7 @@ class ExpoMF(BaseEstimator, TransformerMixin):
         self.b = float(kwargs.get('b', 1.0))
 
     def _init_params(self, n_users, n_items):
+        ''' Initialize all the latent factors '''
         self.theta = self.init_std * \
             np.random.randn(n_users, self.n_components).astype(floatX)
         self.beta = self.init_std * \
@@ -61,6 +109,24 @@ class ExpoMF(BaseEstimator, TransformerMixin):
         self.mu = self.init_mu * np.ones(n_items, dtype=floatX)
 
     def fit(self, X, vad_data=None, **kwargs):
+        '''Fit the model to the data in X.
+
+        Parameters
+        ----------
+        X : scipy.sparse.csr_matrix, shape (n_users, n_items)
+            Training data.
+
+        vad_data: scipy.sparse.csr_matrix, shape (n_users, n_items)
+            Validation data.
+
+        **kwargs: dict
+            Additional keywords to evaluation function call on validation data
+
+        Returns
+        -------
+        self: object
+            Returns the instance itself.
+        '''
         n_users, n_items = X.shape
         self._init_params(n_users, n_items)
         self._update(X, vad_data, **kwargs)
@@ -70,6 +136,7 @@ class ExpoMF(BaseEstimator, TransformerMixin):
         pass
 
     def _update(self, X, vad_data, **kwargs):
+        '''Model training and evaluation on validation set'''
         n_users = X.shape[0]
         XT = X.T.tocsr()  # pre-compute this
         old_ndcg = -np.inf
@@ -131,6 +198,7 @@ class ExpoMF(BaseEstimator, TransformerMixin):
         pass
 
     def _save_params(self, iter):
+        '''Save the parameters'''
         if not os.path.exists(self.save_dir):
             os.makedirs(self.save_dir)
         filename = 'ExpoMF_K%d_mu%.1e_iter%d.npz' % (self.n_components,
@@ -139,6 +207,8 @@ class ExpoMF(BaseEstimator, TransformerMixin):
                  V=self.beta, mu=self.mu)
 
 
+# Utility functions #
+
 def _writeline_and_time(s):
     sys.stdout.write(s)
     sys.stdout.flush()
@@ -146,11 +216,14 @@ def _writeline_and_time(s):
 
 
 def get_row(Y, i):
+    '''Given a scipy.sparse.csr_matrix Y, get the values and indices of the
+    non-zero values in i_th row'''
     lo, hi = Y.indptr[i], Y.indptr[i + 1]
     return Y.data[lo:hi], Y.indices[lo:hi]
 
 
 def a_row_batch(Y_batch, theta_batch, beta, lam_y, mu):
+    '''Compute the posterior of exposure latent variables A by batch'''
     pEX = sqrt(lam_y / 2 * np.pi) * \
         np.exp(-lam_y * theta_batch.dot(beta.T)**2 / 2)
     A = (pEX + EPS) / (pEX + EPS + (1 - mu) / mu)
@@ -159,6 +232,7 @@ def a_row_batch(Y_batch, theta_batch, beta, lam_y, mu):
 
 
 def _solve(k, A_k, X, Y, f, lam, lam_y, mu):
+    '''Update one single factor'''
     s_u, i_u = get_row(Y, k)
     a = np.dot(s_u * A_k[i_u], X[i_u])
     B = X.T.dot(A_k[:, np.newaxis] * X) + lam * np.eye(f)
@@ -166,6 +240,8 @@ def _solve(k, A_k, X, Y, f, lam, lam_y, mu):
 
 
 def _solve_batch(lo, hi, X, X_old_batch, Y, m, f, lam, lam_y, mu):
+    '''Update factors by batch, will eventually call _solve() on each factor to
+    keep the parallel process busy'''
     assert X_old_batch.shape[0] == hi - lo
 
     if mu.size == X.shape[0]:  # update users
@@ -181,10 +257,9 @@ def _solve_batch(lo, hi, X, X_old_batch, Y, m, f, lam, lam_y, mu):
 
 
 def recompute_factors(X, X_old, Y, lam, lam_y, mu, n_jobs, batch_size=1000):
-    '''
-    regress X to Y with exposure matrix A and ridge term lam
-    all the comments below are in the view of computing user factors
-    '''
+    '''Regress X to Y with exposure matrix (computed on-the-fly with X_old) and
+    ridge term lam by embarrassingly parallelization. All the comments below
+    are in the view of computing user factors'''
     m, n = Y.shape  # m = number of users, n = number of items
     assert X.shape[0] == n
     assert X_old.shape[0] == m
